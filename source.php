bacfcefba60f443e338eda20d2eb3d468e398b5b
MissVeronica <veronica@tranza.se>
2021-07-12 18:15:14 +0200

    Update source.php

--- source.php
+++ source.php
@@ -9,6 +9,13 @@ function my_submit_account_details( $args ) {
 
     $current_tab = isset( $_POST['_um_account_tab'] ) ? $_POST['_um_account_tab']: '';
     if ( 'delete' == $current_tab && um_user( 'can_delete_profile' )) {
+        UM()->mail()->send( um_user( 'user_email' ), 'deletion_email' );
+        $emails = um_multi_admin_email();
+        if ( ! empty( $emails ) ) {
+            foreach ( $emails as $email ) {
+                UM()->mail()->send( $email, 'notification_deletion', array( 'admin' => true ) );
+            }
+        }
         UM()->roles()->set_role( um_user('ID'), 'um_archived' );
         wp_logout();
         um_redirect_home();
