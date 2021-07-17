a4e2b24024c9639ea3156fc9ee7805620f382420
MissVeronica <veronica@tranza.se>
2021-07-17 11:28:44 +0200

    Update source.php

--- source.php
+++ source.php
@@ -17,7 +17,9 @@ function my_submit_account_details( $args ) {
             }
         }
         update_user_meta( um_user('ID'), 'account_status', 'archived' );
-        UM()->roles()->set_role( um_user('ID'), 'um_archived' );
+        //UM()->roles()->set_role( um_user('ID'), 'um_archived' );
+        $user = new \WP_User( um_user('ID') );
+	    $user->set_role( 'um_archived' );
         delete_option( "um_cache_userdata_" . um_user('ID'));
         wp_logout();
         um_redirect_home();
