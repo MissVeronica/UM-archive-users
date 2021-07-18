8bc09889cfd1a1491c7b388157f9b97d4ff27217
MissVeronica <veronica@tranza.se>
2021-07-18 17:34:16 +0200

    Update source.php

--- source.php
+++ source.php
@@ -4,6 +4,7 @@ add_action( 'um_submit_account_details', 'my_submit_account_details', 9, 1 );
 add_action( 'um_user_login', 'my_user_login', 9, 1 );
 add_action( 'um_profile_content_main', 'my_profile_content_main', 9, 1 );
 add_filter( 'um_profile_can_view_main', 'my_profile_can_view_main', 10, 2 );
+add_action( 'wp_login', 'my_wp_user_login', 10, 2 );
 
 function my_submit_account_details( $args ) {
 
@@ -18,7 +19,7 @@ function my_submit_account_details( $args ) {
         }
         update_user_meta( um_user('ID'), 'account_status', 'archived' );
         //UM()->roles()->set_role( um_user('ID'), 'um_archived' );
-        wp_update_user( array( 'ID' => um_user('ID'), 'role' => 'um_archived' ));
+	    wp_update_user( array( 'ID' => um_user('ID'), 'role' => 'um_archived' ) );
         delete_option( "um_cache_userdata_" . um_user('ID'));
         wp_logout();
         um_redirect_home();
@@ -31,6 +32,11 @@ function my_user_login( $args ) {
         wp_logout();
         um_redirect_home();
     }    
+}
+
+function my_wp_user_login( $user_login, $user ) {
+
+    if( in_array( "um_archived", $user->roles )) wp_logout();
 }
 
 function my_profile_content_main( $args ) {
